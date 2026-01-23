c1b51624d2b1b83d06c5ab868c7a714d84dd2396
sirmaelstrom <jmheath@gmail.com>
2026-01-22 20:57:42 -0600

    chore(01-01): create Blazor Server project with EF Core PostgreSQL
    - Create new Blazor Server project with interactivity mode
    - Add Npgsql.EntityFrameworkCore.PostgreSQL package
    - Add Microsoft.EntityFrameworkCore.Design package
    - Add Bogus package for development seed data
    - Register DbContextFactory in Program.cs for Blazor Server thread safety
    - Configure default connection string with fallback for development

---
+++ src/FamilyCoordinationApp/Program.cs
@@ -0,0 +1,36 @@
+using FamilyCoordinationApp.Components;
+using FamilyCoordinationApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Database configuration - DbContextFactory for Blazor Server thread safety
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? "Host=localhost;Database=familyapp;Username=familyapp;Password=***REDACTED***";
+
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
