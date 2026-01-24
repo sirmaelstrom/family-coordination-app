ac5b0ddb1423b2a54a004de38ab63d707cc60c4f
sirmaelstrom <jmheath@gmail.com>
2026-01-23 19:13:45 -0600

    feat(02-01): add Category entity with soft delete
    - Create Category entity with composite key (HouseholdId, CategoryId)
    - Add global query filter for soft delete on Category
    - Add Notes and GroupName fields to RecipeIngredient
    - Create CategoryConfiguration with unique index on household/name
    - Add Categories DbSet to ApplicationDbContext

---
+++ src/FamilyCoordinationApp/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using FamilyCoordinationApp.Data.Entities;
+
+namespace FamilyCoordinationApp.Data.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(c => new { c.HouseholdId, c.CategoryId });
+
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(c => c.IconEmoji)
+            .HasMaxLength(10);
+
+        builder.Property(c => c.Color)
+            .HasMaxLength(7);  // #FFFFFF format
+
+        builder.HasQueryFilter(c => !c.IsDeleted);
+
+        builder.HasOne(c => c.Household)
+            .WithMany()
+            .HasForeignKey(c => c.HouseholdId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(c => new { c.HouseholdId, c.Name })
+            .IsUnique()
+            .HasFilter("\"IsDeleted\" = false");
+    }
+}
